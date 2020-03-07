83d6af9fac9189fd8284bda0d2558de7647beeb3
lifan <3286150043@qq.com>
2020-03-07 17:27:16 +0800

    对比实现的lstm与官方lstm的差别

--- lstm.py
+++ lstm.py
@@ -4,21 +4,19 @@ from tensorflow.python.keras import backend as K
 from tensorflow.python.ops import array_ops
 from tensorflow.python.keras import activations
 from tensorflow.python.util.tf_export import keras_export
-import rnn
 
 class LSTM_CELL(tf.keras.layers.Layer):
-    def __init__(self, unites=256, **kwargs):
+    def __init__(self, units=256, **kwargs):
         # lstm 维度
         self.units = units
-        self.state_size = unites
         super(LSTM_CELL, self).__init__(**kwargs)
 
     def build(self, input_shape):
         input_dim = input_shape[-1]
-        self.kernel = self.add_weight(shape=(input_dim, self.units * 4), name='kernel',
+        self.w = self.add_weight(shape=(input_dim, self.units * 4), name='kernel',
             initializer=initializers.get('glorot_uniform'))
 
-        self.recurrent_kernel = self.add_weight(shape=(self.units, self.units * 4),
+        self.u = self.add_weight(shape=(self.units, self.units * 4),
                                                 name='recurrent_kernel',
                                                 initializer=initializers.get('orthogonal'))
         self.bias = self.add_weight(
@@ -28,43 +26,57 @@ class LSTM_CELL(tf.keras.layers.Layer):
         self.recurrent_activation = activations.get('hard_sigmoid')
         self.activation = activations.get('tanh')
 
-
     def call(self, inputs, states):
         last_h = states[0]
         last_c = states[1]
-        os._exit(0)
-        k_i, k_f, k_c, k_o = tf.split(self.kernel, num_or_size_splits=4, axis=1)
+        w_i, w_f, w_c, w_o = tf.split(self.w, num_or_size_splits=4, axis=1)
         b_i, b_f, b_c, b_o = tf.split(self.bias, num_or_size_splits=4, axis=0)
         # w x
-        x_i = K.dot(inputs, k_i)
-        x_f = K.dot(inputs, k_f)
-        x_c = K.dot(inputs, k_c)
-        x_o = K.dot(inputs, k_o)
+        x_i = K.dot(inputs, w_i)
+        x_f = K.dot(inputs, w_f)
+        x_c = K.dot(inputs, w_c)
+        x_o = K.dot(inputs, w_o)
         # w x + b
         x_i = K.bias_add(x_i, b_i)
         x_f = K.bias_add(x_f, b_f)
         x_c = K.bias_add(x_c, b_c)
         x_o = K.bias_add(x_o, b_o)
 
-        k_i, k_f, k_c, k_o = tf.split(self.recurrent_kernel, num_or_size_splits=4, axis=1)
+        u_i, u_f, u_c, u_o = tf.split(self.u, num_or_size_splits=4, axis=1)
         # w x + u * h + x
-        i = self.recurrent_activation(x_i + K.dot(last_h, k_i))
-        f = self.recurrent_activation(x_f + K.dot(last_h, k_f))
-        c = f * last_c + self.activation(x_c + K.dot(last_h, k_c))
-        o = self.recurrent_activation(x_o + K.dot(last_h, k_o))
+        i = self.recurrent_activation(x_i + K.dot(last_h, u_i))
+        f = self.recurrent_activation(x_f + K.dot(last_h, u_f))
+        c = f * last_c + self.activation(x_c + K.dot(last_h, u_c))
+        o = self.recurrent_activation(x_o + K.dot(last_h, u_o))
 
         # 计算 h
         h = o * self.activation(c)
-        return h, [h, c]
+        return h, (h, c)
+
+class Rnn(tf.keras.layers.Layer):
+    def __init__(self, units=128):
+        super(Rnn, self).__init__()
+        self.cell = LSTM_CELL(units)
+        self.init_state = None
+    def build(self, input_shape):
+        shape = input_shape.as_list()
+        n_batch = shape[0]
+        init_h = tf.zeros(shape=[n_batch, self.cell.units])
+        init_c = init_h
+        self.init_state = (init_h, init_c)
+
+    def call(self, inputs):
+        # time step
+        ts = inputs.shape.as_list()[1]
+        h, c = self.init_state
+        for i in range(ts):
+            h, (h, c) = self.cell(inputs[:, i], (h, c))
+        return h
 
 if __name__ == "__main__":
-    units = 128
-    lstm = LSTM_CELL(units)
-    init_h = tf.zeros(shape=(100, units))
-    init_c = init_h
     a = tf.random.normal(shape=(100, 10, 50))
-    rnn = rnn.RNN(lstm)
-    y = rnn(a, [init_h, init_c])
-    print(y.shape)
+    rnn = Rnn(128)
+    h = rnn(a)
+    print(h.shape)
 
 
--- test.py
+++ test.py
@@ -3,27 +3,39 @@ from tensorflow import keras
 import lstm
 
 import numpy as np
-import matplotlib.pyplot as plt
 import os
-
+import time
+import pickle
 
 class MyModel(tf.keras.Model):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.flatten = tf.keras.layers.Flatten()
+        #self.flatten = tf.keras.layers.Flatten()
+        #self.rnn = tf.keras.layers.RNN(tf.keras.layers.LSTMCell(256))
+        self.rnn = lstm.Rnn(256)
         self.d1 = tf.keras.layers.Dense(128, activation="relu")
         self.d2 = tf.keras.layers.Dense(10, activation="softmax")
     def call(self, x):
-        print(x.shape)
-        os._exit(0)
-        # [batch_size, height*width], [4, 784]
-        x = self.flatten(x)
-        x 
+        x = self.rnn(x)
         # [batch_size, d1.output_size], [4, 128]
         x = self.d1(x)
         # [batch_size, d2.output_size], [4, 10]
         x = self.d2(x)
         return x
+
+
+@tf.function
+def train_step(model, loss, opti, images, labels, train_loss, train_acc):
+    with tf.GradientTape() as tape:
+        # pred [batch_size, n_class] (4, 10)
+        pred = model(images)
+        loss_val = loss(labels, pred)
+    train_loss.update_state(loss_val)
+    train_acc.update_state(labels, pred)
+    grad = tape.gradient(loss_val, model.trainable_variables)
+    opti.apply_gradients(zip(grad, model.trainable_variables))
+
+
 
 def train():
     # 定义优化器
@@ -38,26 +50,34 @@ def train():
     fashion_mnist = keras.datasets.fashion_mnist
     (train_images, train_labels), _ = fashion_mnist.load_data()
     train_images = train_images / 255.0
+    num_used = 5000
+    train_images = train_images[:num_used]
+    train_labels = train_labels[:num_used]
     train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(10000).batch(4)
     # 定义模型
     model = MyModel()
     epochs = 5
+
+    list_time_cost = list()
+    list_acc = list()
     for epoch in range(epochs):
         # train
         train_loss.reset_states()
         train_acc.reset_states()
         # images [batch_size, height, width] (4, 28, 28)
         # labels [batch_size]
+        start = time.time()
         for images, labels in train_ds:
-            with tf.GradientTape() as tape:
-                # pred [batch_size, n_class] (4, 10)
-                pred = model(images)
-                loss_val = loss(labels, pred)
-                train_loss.update_state(loss_val)
-                train_acc.update_state(labels, pred)
-            grad = tape.gradient(loss_val, model.trainable_variables)
-            opti.apply_gradients(zip(grad, model.trainable_variables))
-        print("Epoch: {}, loss: {}, acc: {}".format(epoch, train_loss.result(), train_acc.result()), end=", ")
+            train_step(model, loss, opti, images, labels, train_loss, train_acc)
+        ends = time.time()
+        cost = ends - start
+        list_time_cost.append(cost)
+        list_acc.append(train_acc.result().numpy())
+        print("Time: {} s, Epoch: {}, loss: {}, acc: {}".format(cost, epoch, train_loss.result(), train_acc.result()))
+    with open("./data/lstm_acc.pkl", "wb") as fw:
+        pickle.dump(list_acc, fw)
+    with open("./data/lstm_time_cost.pkl", "wb") as fw:
+        pickle.dump(list_time_cost, fw)
 
 if __name__ == "__main__":
     train()
